63b4c474b319da2b3889162de313879106024408
eugenia ong <eugenia.osy@gmail.com>
2019-05-06 13:45:04 -0700

    Refactor and transferred all js code to app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ class Board {
     return this.count % 2 === 0 ? this.player1 : this.player2;
   }
 
-  updateAndCheck(row, col) {
+  checkWin(row, col) {
     var player = this.getPlayer();
     if (++player.rows[row] === 3) {
       return true;
@@ -52,10 +52,55 @@ class Board {
 };
 
 // current board
-var currentGame = new Board();
-var players = ['', ''];
+var currentGame;
+var player1 = '';
+var player2 = '';
 
 // set up a new board for each game
 var startNewGame = function () {
   currentGame = new Board();
+  currentGame.player1.name = player1;
+  currentGame.player2.name = player2;
+
+  var currentPlayer = document.getElementById('turn');
+  currentPlayer.innerText = player1 + ': you start!';
 };
+
+// clear up the tiles for new board game
+var clearTiles = function () {
+  var tiles = document.getElementsByClassName('tile');
+  for (let tile of tiles) {
+    tile.innerText = '';
+  }
+}
+
+// update display and board details when tile is clicked on
+var updateGame = function (tile) {
+  if (tile.innerText) {
+    alert('Invalid selection! Please pick again');
+    return;
+  }
+
+  var mark = currentGame.count % 2 === 0 ? 'X' : 'O';
+  tile.innerText = mark;
+
+  var row = tile.classList[1].substring(5, 6);
+  var col = tile.classList[1].substring(7, 8);
+  currentGame.toggle(row, col, mark);
+
+  if (currentGame.checkWin(row, col)) {
+    var player = currentGame.getPlayer().name;
+    alert(`Congrats! ${player} has won the game!`);
+    return;
+  }
+
+  if (++currentGame.count > 9) {
+    alert('No winner. Try again!');
+  }
+}
+
+var updateTurn = function () {
+  var currentPlayer = document.getElementById('turn');
+  var player = currentGame.getPlayer().name;
+  currentPlayer.innerText = player + ': your turn!';
+} 
