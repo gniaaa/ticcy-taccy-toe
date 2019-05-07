edbfa7ce43223dcb674baea1de0578942204f559
eugenia ong <eugenia.osy@gmail.com>
2019-05-06 17:05:31 -0700

    Refactor view into class component and remove global variables

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Board {
-  constructor(props) {
+  constructor() {
     this.board = [[' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']];
@@ -44,81 +44,98 @@ class Board {
       return true;
     }
 
-    if (row + col === 2 && ++player.min === 3) {
+    if (Number(row) + Number(col) === 2 && ++player.min === 3) {
       return true;
     }
     return false;
   }
 };
 
+class View {
+  constructor() {
+  }
 
-// object to store current
+  startNewGame() {
+    currentGame.board.player1.name = currentGame.player1;
+    currentGame.board.player2.name = currentGame.player2;
 
-// current board
-var currentGame;
-var player1 = '';
-var player2 = '';
+    var currentPlayer = document.getElementById('turn');
+    currentPlayer.innerText = currentGame.board.player1.name + ': you start!';
+  }
 
-// object for presentation
+  clearTiles() {
+    var tiles = document.getElementsByClassName('tile');
+    for (let tile of tiles) {
+      tile.innerText = '';
+    }
+  }
 
+  updateGame(tile) {
+    if (tile.innerText) {
+      alert('Invalid selection! Please pick again');
+      return;
+    }
 
-// set up a new board for each game
-var startNewGame = function () {
-  currentGame = new Board();
-  currentGame.player1.name = player1;
-  currentGame.player2.name = player2;
+    var mark = currentGame.board.count % 2 === 0 ? 'X' : 'O';
+    tile.innerText = mark;
 
-  var currentPlayer = document.getElementById('turn');
-  currentPlayer.innerText = player1 + ': you start!';
+    var row = tile.classList[1].substring(5, 6);
+    var col = tile.classList[1].substring(7, 8);
+    currentGame.board.toggle(row, col, mark);
+
+    if (currentGame.board.checkWin(row, col)) {
+      var player = currentGame.board.getPlayer().name;
+      alert(`Congrats! ${player} has won the game!`);
+      if (player === currentGame.player1) {
+        currentGame.player1Score++;
+      } else {
+        currentGame.player2Score++;
+      }
+      currentGame.view.displayPlayerNamesAndScores();
+      return;
+    }
+
+    if (++currentGame.board.count > 9) {
+      alert('No winner. Try again!');
+    }
+  }
+
+  updateTurn() {
+    var currentPlayer = document.getElementById('turn');
+    var player = currentGame.board.getPlayer().name;
+    currentPlayer.innerText = player + ': your turn!';
+  }
+
+  getPlayerNames() {
+    currentGame.player1 = (prompt('Player 1\'s name: ') || 'husky').toLowerCase();
+    currentGame.player2 = (prompt('Player 2\'s name: ') || 'pomeranian').toLowerCase();
+  }
+
+  displayPlayerNamesAndScores() {
+    var p1 = document.getElementById("player1");
+    p1.innerText = `player 1: ${currentGame.player1}, score: ${currentGame.player1Score}`;
+
+    var p2 = document.getElementById("player2");
+    p2.innerText = `player 2: ${currentGame.player2}, score: ${currentGame.player2Score}`;
+  }
+
+}
+
+var currentGame = {
+  board: [],
+  view: {},
+  player1: '',
+  player2: '',
+  player1Score: 0,
+  player2Score: 0
 };
 
-// clear up the tiles for new board game
-var clearTiles = function () {
-  var tiles = document.getElementsByClassName('tile');
-  for (let tile of tiles) {
-    tile.innerText = '';
-  }
-}
-
-// update display and board details when tile is clicked on
-var updateGame = function (tile) {
-  if (tile.innerText) {
-    alert('Invalid selection! Please pick again');
-    return;
-  }
-
-  var mark = currentGame.count % 2 === 0 ? 'X' : 'O';
-  tile.innerText = mark;
-
-  var row = tile.classList[1].substring(5, 6);
-  var col = tile.classList[1].substring(7, 8);
-  currentGame.toggle(row, col, mark);
-
-  if (currentGame.checkWin(row, col)) {
-    var player = currentGame.getPlayer().name;
-    alert(`Congrats! ${player} has won the game!`);
-    return;
-  }
-
-  if (++currentGame.count > 9) {
-    alert('No winner. Try again!');
-  }
-}
-
-var updateTurn = function () {
-  var currentPlayer = document.getElementById('turn');
-  var player = currentGame.getPlayer().name;
-  currentPlayer.innerText = player + ': your turn!';
-}
 
 window.onload = (event) => {
-  player1 = (prompt('Player 1\'s name: ') || 'player1').toLowerCase();
-  player2 = (prompt('Player 2\'s name: ') || 'player2').toLowerCase();
+  currentGame.board = new Board();
 
-  var p1 = document.getElementById("player1");
-  p1.innerText = 'player 1: ' + player1;
-
-  var p2 = document.getElementById("player2");
-  p2.innerText = 'player 2: ' + player2;
-  startNewGame();
+  currentGame.view = new View();
+  currentGame.view.getPlayerNames();
+  currentGame.view.displayPlayerNamesAndScores();
+  currentGame.view.startNewGame();
 };
